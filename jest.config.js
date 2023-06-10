50a7ddf6dd1ae640f7128b9b7f05e9bafcbd6233
levi <levidcd@outlook.com>
2023-06-10 11:04:10 +0800

    test: 💍 添加jestjest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^lodash-es$': 'lodash'
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true
+        // tsconfig: 'tsconfig.test.json'
+      }
+    ]
+  },
+  setupFiles: ['./setupJest.js']
+}
